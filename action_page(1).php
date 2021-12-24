06d9298f8e65efc582a1d1dac3d3e3499a62c7d4
Nagendra Chouhan <nikku200109@gmail.com>
2021-12-25 00:37:30 +0530

    create css ans js folder "

--- action_page(1).php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h3>"Your have been searched for:"</h3>
-:<?php echo $_POST["search"] ;?><br>
-</body>
-</html>
--- action_page.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<h3>"Your input has been recieved as:"</h3>
-Username:<?php echo $_POST["uname"] ;?><br>
-Password:<?php echo $_POST["psw"] ;?><br>
-</body>
-</html>
