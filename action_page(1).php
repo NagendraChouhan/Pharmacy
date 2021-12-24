d552bb4010705d85faca6f1c859e763c6309b580
Nagendra Chouhan <nikku200109@gmail.com>
2021-12-24 19:34:43 +0530

    my first commit

---
+++ action_page(1).php
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h3>"Your have been searched for:"</h3>
+:<?php echo $_POST["search"] ;?><br>
+</body>
+</html>
---
+++ action_page.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h3>"Your input has been recieved as:"</h3>
+Username:<?php echo $_POST["uname"] ;?><br>
+Password:<?php echo $_POST["psw"] ;?><br>
+</body>
+</html>
